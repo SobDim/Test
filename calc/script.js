452d1f37bb04274d68758ea8dca0fd1548728c6a
SobDim <sdswit@gmail.com>
2022-04-04 17:23:28 +0300

    1 тест

---
+++ calc/script.js
@@ -0,0 +1,14 @@
+// let a = "";
+// let b = "";
+// let result = document.querySelector('result');
+// let operation = "";
+// //document.querySelector('.buttons')
+
+// document.querySelector('.buttuns').addEventListener('click', (event) => {
+//         result.innerText = event.target.innerText;
+//         console.log(result);
+//     });
+
+// 5
+console.log({});
+console.log([]);
